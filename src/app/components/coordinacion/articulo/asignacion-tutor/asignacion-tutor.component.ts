7694d58a922ec9ad5e44de0e9925db171d42587e
Jesus Sierra <jnsierrac@gmail.com>
2021-08-15 20:27:48 -0500

    Ajustes para la publicación de articulos

--- src/app/components/coordinacion/articulo/asignacion-tutor/asignacion-tutor.component.ts
+++ src/app/components/coordinacion/articulo/asignacion-tutor/asignacion-tutor.component.ts
@@ -1,3 +1,6 @@
+import { UtilesBase64Service } from './../../../../servicios/utilesBase64.service';
+import { DocumentDownload } from './../../../../models/DocumentDownload.model';
+import { DownloadService } from './../../../../servicios/download.service';
 import { ArticuloModel } from './../../../../models/articulo.model';
 import { ArticulosService } from 'src/app/servicios/articulos.service';
 import { DibujaProcesoComponent } from './../../../general/dibuja-proceso/dibuja-proceso.component';
@@ -6,6 +9,7 @@ import { IdeaModel } from 'src/app/models/idea.model';
 import { IdeaService } from 'src/app/servicios/idea.service';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-asignacion-tutor',
@@ -21,6 +25,8 @@ export class AsignacionTutorComponent implements OnInit {
     private _articulosService: ArticulosService,
     private dialog: MatDialog,
     private router: Router,
+    private _downloadFiles: DownloadService,
+    private _utilesBase64Service: UtilesBase64Service
     ) {
     this.listIdeas = new Array(0);
     this.listaArticulos = new Array(0);
@@ -53,6 +59,35 @@ export class AsignacionTutorComponent implements OnInit {
 
   verFlujo(id: number){
     const dialogRef = this.dialog.open(DibujaProcesoComponent, {data:{idIdea: id}});
+  }
+
+  verCarta(urlCarta: string){
+    console.log(urlCarta)
+    let documento = new DocumentDownload();
+    documento.ubicacion = urlCarta;
+
+    this._downloadFiles.getDocumentByUbicacion(documento).subscribe( resp => {
+      if(resp){
+        resp.nombre = "carta.pdf";
+        this._utilesBase64Service.downloadPdf(resp.document, resp.nombre);
+      }
+    });
+  }
+
+  publicarArticulo(idArt: number){
+    this._articulosService.actualizarEstadoArticulo(idArt, "PUBLICADO").subscribe( resp => {
+      if(resp){
+        Swal.fire({
+          allowOutsideClick: false,
+          type: 'success',
+          text: 'Articulo publicado de forma correcta'
+        }).then((result) => {
+          if (result.value) {
+            this.router.navigateByUrl('/publico');
+          }
+        });
+      }
+    });
   }
 
 }
